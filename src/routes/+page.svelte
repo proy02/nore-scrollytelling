e2a68b8a8b4b5d4ded4b194d9cc2b2cad179ae01
apple <apple@MacBook-Pro.local>
2026-02-23 14:37:41 +0530

    .intro text edit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -608,21 +608,21 @@
       <div class="info-panel intro-info-panel">
         <h1>The Three Sisters of <br> Southeast Ireland <br></h1>
         
-        <p><br>A Living Catchment Story<br>
-        <br>This is not a finished story.<br>
-        <br>It is a beginning.<br>
-        <br>What you are about to scroll through is a scaffold — a structure to help us see, sense,
-            and speak about the Three Sisters catchment of southeast Ireland: the Nore, the Suir, and the Barrow,
-            and the lands and lives they connect.<br>
-        <br>This story is a primer — for curiosity, for care, and for citizen science.
-            It offers shared reference points: rivers and tributaries, soils and species,
-            pressures and protections. But it does not pretend to hold the whole truth.<br>
-        <br>A catchment is not only a hydrological unit.<br>
-        <br>It is a lived place and lived places are known through memory, observation, labour, and life.
-            As you move through these layers — maps, stanzas, data, and images — you are invited to notice where
-            you are in the system. Explore how land, water and life interact.  To reflect on what you’ve seen change.
-            What you’ve inherited. What you worry about. What you hope for.<br>
-        <br> This story is designed to be entered, contemplated — and then rewritten, with you. <br> </p>
+        <p>A Living Catchment Story</p> 
+        <p>This is not a finished story.</p>
+        <p>It is a beginning.</p>
+        <p>What you are about to scroll through is a scaffold — a structure to help us see, sense,
+          and speak about the Three Sisters catchment of southeast Ireland: the Nore, the Suir, and the Barrow,
+          and the lands and lives they connect.</p>
+          <p> This story is a primer — for curiosity, for care, and for citizen science.
+              It offers shared reference points: rivers and tributaries, soils and species,
+              pressures and protections. But it does not pretend to hold the whole truth.</p>
+          <p>A catchment is not only a hydrological unit.</p>
+          <p>It is a lived place and lived places are known through memory, observation, labour, and life.</p>
+          <p>As you move through these layers — maps, stanzas, data, and images — you are invited to notice where
+           you are in the system. Explore how land, water and life interact.  To reflect on what you’ve seen change.
+           What you’ve inherited. What you worry about. What you hope for.</p>
+          <p>This story is designed to be entered, contemplated — and then rewritten, with you.</p>
 
         <p class="keep-scrolling"> Keep Scrolling ↓</p>
       </div>
@@ -974,7 +974,7 @@
   }
 
   .intro-info-panel p {
-    margin: 0;
+    margin: 0 0 1rem 0;  /* adjust this number */;
     color: #232323;
     line-height: 1.5;
     font-size: 1.1rem;

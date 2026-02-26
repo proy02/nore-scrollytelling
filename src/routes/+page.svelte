725554c2b08db106a143a65a8b506fc760a80436
apple <apple@MacBook-Pro.local>
2026-02-26 17:48:10 +0530

    blur - alignment debug

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -733,7 +733,7 @@
     position: absolute;
     left: 0;
     top: 0;
-    width: 130px;
+    width: 150px;
     height: 100%;
     background: linear-gradient(to right, #ffffff, transparent);
     z-index: 11;  /* above the blur */
